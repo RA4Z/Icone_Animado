0a37660a22702f6aa0650b3102201a46ea95facd
RA4Z <rraz639@gmail.com>
2023-04-29 14:15:16 -0300

    Projeto criado para aprender a manipular ícones com animação

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import { Feather } from 'react-native-vector-icons'
+import LottieView from 'lottie-react-native'
 
 export default function App() {
+  let animation = React.createRef();
+
+  function startAnimation() {
+    animation.current.play();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={startAnimation} style={{marginTop: 200}}>
+        <Feather name="play" size={60} color="black" />
+      </TouchableOpacity>
+
+      <LottieView 
+        source={require('./assets/check.json')}
+        style={{
+          width: 500,
+          height: 500,
+        }}
+        loop={false}
+        ref={animation}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
